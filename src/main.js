cf35cf8c0caefee3c90be316a1968501cd37589d
Elena Chugunova <ches77@inbox.ru>
2020-05-03 14:38:00 +0300

    first commit

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "./styles/main.pcss";
-if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
-}
-
-import "./scripts/skills";
-import "./scripts/works";
-import "./scripts/projects-slider";
-import "./scripts/parallax";
