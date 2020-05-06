61fbb7671122a1185f596a8657cccf587548cace
Elena Chugunova <ches77@inbox.ru>
2020-05-06 12:16:04 +0300

    'version_two'

---
+++ src/scripts/parallax.js
@@ -0,0 +1,30 @@
+var parallax=(function () {
+    var bg= document.getElementById("herobg");
+    var stars=document.getElementById('stars');
+    var hands=document.getElementById('hands');
+    var user=document.getElementById('hero');
+    return{
+        move:function (block,windowScroll, strafeAmount) {
+
+            var strafe=windowScroll/-strafeAmount +'%' ;
+            var transformString='translate3d(0,' + strafe +', 0)';
+            var style = block.style;
+
+            style.transform =transformString;
+            style.webkitTransform = transformString;
+         },
+        init: function (wScroll) {
+           // this.move(bg,wScroll,45);
+            this.move(hands,wScroll,30);
+            this.move(stars,wScroll,20);
+            this.move(user,wScroll,10);
+
+        }
+    }
+}());
+
+ window.onscroll=function () {
+    console.log(222222);
+     var wScroll=window.pageYOffset;
+     parallax.init(wScroll);
+}
