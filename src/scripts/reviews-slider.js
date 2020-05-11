5e283a9c1149bf0d3add15c8a02ae95b09e4d829
Elena Chugunova <ches77@inbox.ru>
2020-05-11 12:38:51 +0300

    review-slider 11.05.2020

--- src/scripts/reviews-slider.js
+++ src/scripts/reviews-slider.js
@@ -1,6 +1,4 @@
 import Vue from "vue";
-
-
 import Flickity from 'vue-flickity';
 
 new Vue({
@@ -19,7 +17,7 @@ new Vue({
                 prevNextButtons: false,
                 pageDots: false,
                 wrapAround: false,
-                groupCells: true,
+                groupCells: 2,
                 freeScroll: false,
                 contain: true,
                 reviews: [],
@@ -32,14 +30,46 @@ new Vue({
 
         const data = require("../data/reviews.json");
         this.reviews = data;
+        this.works = this.makeArrWithRequireImages(data);
     },
     methods: {
         next() {
             this.$refs.flickity.next();
+            this.checkArrows();
         },
 
         previous() {
             this.$refs.flickity.previous();
-        }
+            //console.log(this.$ref.flickity.selectedIndex);
+
+            this.checkArrows();
+        },
+        makeArrWithRequireImages(array) {
+            return array.map((item) => {
+                const requirePic = require(`../images/content/${item.photo}`);
+                item.photo = requirePic;
+                return item;
+            });
+        },
+        checkArrows(){
+            console.log(this.$refs.flickity.selectedIndex());
+            console.log(this.$refs.flickity.selectedIndex());
+            if (this.$refs.flickity.selectedIndex() == 0){
+                //document.getElementById("backBtn").disabled="disabled";
+                console.log(111);
+                //document.getElementById("backBtn").classList.remove('review-slider__btn');
+                //.getElementById("backBtn").classList.add('review-slider__btn__notactive');
+                document.getElementById("backBtn").disabled;
+                this.$el.querySelector('.review-slider__btn--next').disabled;
+                //this.$el.querySelector('.review-slider__btn--back').style.display="none";
+
+                //this.$el.querySelector('.review-slider__btn--back').style.cursor= "not-allowed";
+            } else if(this.$refs.flickity.selectedIndex()>=this.$refs.flickity.slides()){
+                this.$el.querySelector('.review-slider__btn--next').style.display="none";
+            } else{
+                this.$el.querySelector('.review-slider__btn--back').style.display="inline-block";
+                this.$el.querySelector('.review-slider__btn--next').style.display="inline-block";
+            }
+        },
     }
 });
