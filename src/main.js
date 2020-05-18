3c8e7174aca7b09161d1f77c0b38ebbd3f6ae321
Elena Chugunova <ches77@inbox.ru>
2020-05-18 10:00:48 +0300

    homework3

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+
 import "./scripts/skills.js";
 import "./scripts/parallax.js";
 import "./scripts/works.js";
--- src/scripts/check-form.js
+++ src/scripts/check-form.js
@@ -1,4 +1,7 @@
 import Vue from "vue";
+import Vuelidate from 'vuelidate';
+Vue.use(Vuelidate);
+import { required, maxLength } from 'vuelidate/lib/validators';
 
 
 new Vue({
@@ -6,27 +9,38 @@ new Vue({
     template: '#reviews-check-template',
     data() {
         return {
+            errorEmail:false,
             errors: [],
             name:null,
             email: null,
             content:null
-
-
         };
+    },
+    validations: {
+        name: {
+            required,
+        },
+        email: {
+            required,
+        },
+        content: {
+            required,
+        },
     },
     methods: {
         checkForm: function (e) {
-            console.log(111);
             this.errors = [];
 
             if (!this.name) {
                 this.errors.push('Укажите имя.');
+                this.errorName=true;
             }
             if (!this.content) {
                 this.errors.push('ведите текст.');
             }
             if (!this.email) {
                 this.errors.push('Укажите электронную почту.');
+                this.errorEmail=true;
             } else if (!this.validEmail(this.email)) {
                 this.errors.push('Укажите корректный адрес электронной почты.');
             }
@@ -36,8 +50,13 @@ new Vue({
             }
 
             e.preventDefault();
-            if (this.errors.length){
-                alert("Пожалуйста, исправьте ошибки!: \n"+this.errors);
+            if (this.errors.length) {
+                alert("Пожалуйста, исправьте ошибки!: \n" + this.errors);
+                //this.errorName=false;
+
+            }
+            else {alert("Форма отправлена")
+
 
             }
             //console.log(this.errors);
